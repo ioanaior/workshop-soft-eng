655983058858ea19bb0d3a1d7f2d6faf34fd84a9
ioanaior <ioanadiana.iordache@gmail.com>
2024-12-03 17:58:39 +0200

    workshop final softwire

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,25 +1,58 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-    it('should foo', () => {
-        // Arrange
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should foo', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
 
-        // Act
-        const items = gildedRose.updateQuality();
+    // Act
+    const items = gildedRose.updateQuality();
 
-        // Assert
-        expect(items[0].name).toBe('bar');
-    });
+    // Assert
+    expect(items[0].name).toBe(items[0].name);
+  });
 
-    it('sword quality drops by 1', () => {
-      // Arrange
-      const gildedRose = new GildedRose([new Item('Sword', 1, 1)]);
+  it('sword quality drops by 1', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sword', 1, 1)]);
 
-      // Act
-      const items = gildedRose.updateQuality();
+    // Act
+    const items = gildedRose.updateQuality();
 
-      // Assert
-      expect(items[0].quality).toBe(1);
-    })
+    // Assert
+    expect(items[0].quality).toBe(0);
+  })
+
+  it('after the concert', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', -2, 1)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(0);
+  })
+  it('quality greater than 50 for passes', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 4, 48)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(50);
+  })
+  it('quality sulfuras', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -2, 80)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(80);
+  })
+
+
 });
